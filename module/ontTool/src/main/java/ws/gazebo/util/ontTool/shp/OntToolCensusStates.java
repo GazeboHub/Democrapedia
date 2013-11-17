6a8ca088ecfe1706d45b5884d75d30e83779b4e1
Sean Champ <spchamp+git-muletop@me.com>
2013-11-17 07:46:40 -0800

    move StateInfo to new file, new package

--- module/ontTool/src/main/java/ws/gazebo/util/ontTool/shp/OntToolCensusStates.java
+++ module/ontTool/src/main/java/ws/gazebo/util/ontTool/shp/OntToolCensusStates.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import ws.gazebo.util.region.state.StateInfo;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class OntToolCensusStates {
@@ -32,45 +33,6 @@ public class OntToolCensusStates {
 			table.add(info);
 		}
 		return table;
-	}
-	
-	public final class StateInfo {
-
-		// cf. http://www.census.gov/geo/reference/ansi_statetables.html
-		// data file available at src/test/resources/census/state.txt
-
-		int stateNumericANSICode; 
-		// ^ xsd:byte length=2, pattern=[0-9]{2}, maxInclusive=78
-		// ^ INCITS 38:200x (previously FIPS 5-2)
-		// ^ PRIMARY KEY
-		String statePostalCode; 
-		// ^ xsd:string length=2, pattern=[A-Z]{2}
-		// ^ UNIQUE
-		String name;
-		// ^ xsd:string
-		// ^ UNIQUE
-		int gnisFeatureCode; 
-		// ^ xsd:byte pattern=[1-9][0-9]{0,9}
-		// ^ UNIQUE, "Alternate key"
-
-		public StateInfo(String[] fields) {
-			stateNumericANSICode = new Integer(fields[0]);
-			statePostalCode = fields[1];
-			name = fields[2];
-			gnisFeatureCode = new Integer(fields[3]);
-		}
-
-		// cf. http://geonames.usgs.gov/domestic/states_fileformat.htm
-		// int gnisFeatureID; // pattern=[1-9][0-9]{0,9} | read string as int
-		// (unsigned)
-		// String gisFeatureName;
-		// - skip feature class = "Civil"
-		// String uspsCode; // length=2, pattern=[A-Z]{2}
-		// byte numericCode; // length=2, pattern=[0-9]{2}, maxInclusive=78 |
-		// INCITS 38:200x (previously FIPS 5-2)
-		// - skip county name (to do: don't sip)
-		// - skip county numeric (to do: don't ski)
-		// - skip remaining fields
 	}
 
 }
