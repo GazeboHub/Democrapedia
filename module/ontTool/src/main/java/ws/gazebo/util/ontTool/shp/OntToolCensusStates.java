abee52b98286e21ec34d2f5a9f951cca7669ba6b
Sean Champ <spchamp+git-muletop@me.com>
2013-11-17 08:23:04 -0800

    use StateInfoTable; cleanup class element imports

--- module/ontTool/src/main/java/ws/gazebo/util/ontTool/shp/OntToolCensusStates.java
+++ module/ontTool/src/main/java/ws/gazebo/util/ontTool/shp/OntToolCensusStates.java
@@ -2,14 +2,11 @@ package ws.gazebo.util.ontTool.shp;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 
-import ws.gazebo.util.region.state.StateInfo;
+import ws.gazebo.util.region.state.StateInfoTable;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class OntToolCensusStates {
-
 
 	public static void main(String[] args) {
 
@@ -17,21 +14,26 @@ public class OntToolCensusStates {
 		// cf. http://opencsv.sourceforge.net/#how-to-read
 		// essentially producing Ontology instance data and properties from the
 		// input value
-		
-		
+
 	}
 
-	public static final Collection<StateInfo> readStateInfo(String pathname)
+	public static final StateInfoTable readStateInfo(String pathname)
 			throws IOException {
 		String[] buffer;
-		StateInfo info;
-		Collection<StateInfo> table = new ArrayList<StateInfo>(57);
-		
-		CSVReader reader = new CSVReader(new FileReader(pathname));
-		while((buffer = reader.readNext()) != null) {
-			info = new StateInfo(buffer);
-			table.add(info);
+		StateInfoTable table = new StateInfoTable();
+
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader(pathname));
+			while ((buffer = reader.readNext()) != null) {
+				table.addStateInfo(buffer);
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
 		}
+
 		return table;
 	}
 
