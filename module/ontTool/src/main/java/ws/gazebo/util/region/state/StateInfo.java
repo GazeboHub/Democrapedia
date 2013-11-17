08d98545d19d33a22d6bd62d7205b2ace2f78245
Sean Champ <spchamp+git-muletop@me.com>
2013-11-17 08:04:08 -0800

    add getter, setter methods

--- module/ontTool/src/main/java/ws/gazebo/util/region/state/StateInfo.java
+++ module/ontTool/src/main/java/ws/gazebo/util/region/state/StateInfo.java
@@ -1,9 +1,15 @@
 package ws.gazebo.util.region.state;
 
+import java.util.Date;
+
 public final class StateInfo {
 
 	// cf. http://www.census.gov/geo/reference/ansi_statetables.html
-	// data file available at src/test/resources/census/state.txt
+	// 
+	// data file available at 
+	// http://www.census.gov/geo/reference/docs/state.txt
+	// locally cached at 
+	// workspace:/democrapedia/module/ontTool/src/test/resources/census/state.txt
 
 	int stateNumericANSICode; 
 	// ^ xsd:byte length=2, pattern=[0-9]{2}, maxInclusive=78
@@ -18,6 +24,13 @@ public final class StateInfo {
 	int gnisFeatureCode; 
 	// ^ xsd:byte pattern=[1-9][0-9]{0,9}
 	// ^ UNIQUE, "Alternate key"
+	
+	// cf. http://nationalatlas.gov/mld/statesp.html
+	byte orderAdmittedUnion;
+	byte dayAdmittedUnion;
+	byte monthAdmittedUnion;
+	short yearAdmittedUnion;
+	
 
 	public StateInfo(String[] fields) {
 		stateNumericANSICode = new Integer(fields[0]);
@@ -26,15 +39,85 @@ public final class StateInfo {
 		gnisFeatureCode = new Integer(fields[3]);
 	}
 
-	// cf. http://geonames.usgs.gov/domestic/states_fileformat.htm
-	// int gnisFeatureID; // pattern=[1-9][0-9]{0,9} | read string as int
-	// (unsigned)
-	// String gisFeatureName;
-	// - skip feature class = "Civil"
-	// String uspsCode; // length=2, pattern=[A-Z]{2}
-	// byte numericCode; // length=2, pattern=[0-9]{2}, maxInclusive=78 |
-	// INCITS 38:200x (previously FIPS 5-2)
-	// - skip county name (to do: don't sip)
-	// - skip county numeric (to do: don't ski)
-	// - skip remaining fields
+
+	public int getStateNumericANSICode() {
+		return stateNumericANSICode;
+	}
+
+
+	public void setStateNumericANSICode(int stateNumericANSICode) {
+		this.stateNumericANSICode = stateNumericANSICode;
+	}
+
+
+	public String getStatePostalCode() {
+		return statePostalCode;
+	}
+
+
+	public void setStatePostalCode(String statePostalCode) {
+		this.statePostalCode = statePostalCode;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getGnisFeatureCode() {
+		return gnisFeatureCode;
+	}
+
+
+	public void setGnisFeatureCode(int gnisFeatureCode) {
+		this.gnisFeatureCode = gnisFeatureCode;
+	}
+
+
+	public byte getDayAdmittedUnion() {
+		return dayAdmittedUnion;
+	}
+
+
+	public void setDayAdmittedUnion(byte dayAdmittedUnion) {
+		this.dayAdmittedUnion = dayAdmittedUnion;
+	}
+
+
+	public byte getMonthAdmittedUnion() {
+		return monthAdmittedUnion;
+	}
+
+
+	public void setMonthAdmittedUnion(byte monthAdmittedUnion) {
+		this.monthAdmittedUnion = monthAdmittedUnion;
+	}
+
+
+	public short getYearAdmittedUnion() {
+		return yearAdmittedUnion;
+	}
+
+
+	public void setYearAdmittedUnion(short yearAdmittedUnion) {
+		this.yearAdmittedUnion = yearAdmittedUnion;
+	}
+
+
+	public byte getOrderAdmittedUnion() {
+		return orderAdmittedUnion;
+	}
+
+
+	public void setOrderAdmittedUnion(byte orderAdmittedUnion) {
+		this.orderAdmittedUnion = orderAdmittedUnion;
+	}
+
+	
 }
