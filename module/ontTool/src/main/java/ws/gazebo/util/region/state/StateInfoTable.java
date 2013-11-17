374049ff4696bdbe36712ca0a41a87489c36245f
Sean Champ <spchamp+git-muletop@me.com>
2013-11-17 08:21:09 -0800

    add constructors; prettify comments

--- module/ontTool/src/main/java/ws/gazebo/util/region/state/StateInfoTable.java
+++ module/ontTool/src/main/java/ws/gazebo/util/region/state/StateInfoTable.java
@@ -10,16 +10,31 @@ public class StateInfoTable {
 	@SuppressWarnings("unused")
 	private static final long serialVersionUID = -1L;
 
-	// 57 entries in the Census ANSI state info table
-	ArrayList<StateInfo> table = new ArrayList<StateInfo>(57);
-	HashMap<Byte, StateInfo> fips52map = new HashMap<Byte, StateInfo>(57);
+	ArrayList<StateInfo> table;
+	HashMap<Byte, StateInfo> fips52map;
+
+	/**
+	 * Initialize table for 57 expected entries.
+	 * 
+	 * @see <a
+	 *      href="http://www.census.gov/geo/reference/ansi_statetables.html">ANSI
+	 *      state table data</a>, US Department of Census.
+	 */
+	public StateInfoTable() {
+		this(57);
+	}
+
+	public StateInfoTable(int expectedEntriesCount) {
+		table = new ArrayList<StateInfo>(expectedEntriesCount);
+		fips52map = new HashMap<Byte, StateInfo>(expectedEntriesCount);
+	}
 
 	/**
 	 * Method for initializing a StateInfo instance based on <a
 	 * href="http://www.census.gov/geo/reference/ansi_statetables.html">ANSI
 	 * state table data</a> from the US Department of Census. This method
-	 * ensures that the instance will be added to all internal indices in the
-	 * table.
+	 * ensures that the instance will be added to the table and all internal
+	 * indices in the table.
 	 * 
 	 * @return new StateInfo instance
 	 */
@@ -33,13 +48,14 @@ public class StateInfoTable {
 
 	/**
 	 * Return the {@link StateInfo} instance denoted uniquely with the
-	 * {@code fips52code} as per INCITS 38:200x (previously FIPS 5-2)
+	 * {@code fips52code} value as per INCITS 38:200x (previously FIPS 5-2)
 	 * 
 	 * @param fips52code
-	 * @return StateInfo instance
+	 *            the INCITS 38:200x code for the instance
+	 * @return {@link StateInfo} instance
 	 */
 	public StateInfo getStateInfo(byte fips52code) {
 		return fips52map.get(fips52code);
 	}
-	
+
 }
