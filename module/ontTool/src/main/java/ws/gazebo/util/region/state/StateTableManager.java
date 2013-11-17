4a70df29e9f2a21ed8357727e1c9a997b03b07bf
Sean Champ <spchamp+git-muletop@me.com>
2013-11-17 09:11:12 -0800

    initialize tableInstance; add static source refs

--- module/ontTool/src/main/java/ws/gazebo/util/region/state/StateTableManager.java
+++ module/ontTool/src/main/java/ws/gazebo/util/region/state/StateTableManager.java
@@ -1,10 +1,23 @@
 package ws.gazebo.util.region.state;
 
-public class StateTableManager {
+import java.io.IOException;
 
-	StateTable tableInstance;
+public class StateTableManager {
+	
+	public static final String STATE_TABLE_SOURCE = "http://www.census.gov/geo/reference/ansi_statetables.html";
+	public static final String STATE_ADM_DATA_SOURCE = "http://nationalatlas.gov/atlasftp.html#statep0";
+
+	private static StateTable tableInstance;
 	
 	public static void main(String[] args) {
+		
+		try {
+			tableInstance = OntToolCensusStates.readStateInfo(args[0]);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(127);
+		}
 		
 		// 1. tableInstance = OntToolCensusStates.readSateInfo(args[0])
 		// 2. Collate the table instance with _ADM data, using OntToolShp
