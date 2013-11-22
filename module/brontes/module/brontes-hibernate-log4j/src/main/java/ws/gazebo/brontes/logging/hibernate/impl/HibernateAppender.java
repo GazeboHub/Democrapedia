a9224194a4f0113226d02a2d108a933d04b57dd0
Sean Champ <spchamp+git-muletop@me.com>
2013-11-22 09:52:41 -0800

    add comments, in context with transaction pooling

--- module/brontes/module/brontes-hibernate-log4j/src/main/java/ws/gazebo/brontes/logging/hibernate/impl/HibernateAppender.java
+++ module/brontes/module/brontes-hibernate-log4j/src/main/java/ws/gazebo/brontes/logging/hibernate/impl/HibernateAppender.java
@@ -5,22 +5,61 @@ import org.apache.log4j.spi.LoggingEvent;
 
 public class HibernateAppender extends AppenderSkeleton {
 
+	/*
+	 * Note: In a database-connected log appender, the concern for transaction
+	 * management (cf. connection pooling) may present something of a
+	 * "tricky matter". Some notes, towards which effect:
+	 * 
+	 * -- Ideally, it should be possible for the application to provide "hints"
+	 * as to the start an end of a single transactional context
+	 * 
+	 * ---- That may be addressed toward supporting a paradigm in which an
+	 * application could "hint" the start of (for instance) an "application
+	 * startup" context, such as on entry into main(), and then to note the end
+	 * of the startup context as on layered entry into regular application
+	 * procedures
+	 * 
+	 * ---- Hypothetically, a set of annotations may be developed within this
+	 * module, such as to support a paradigm of such
+	 * "transaction bounds hinting" namely within the application of the logging
+	 * component of a containing application
+	 * 
+	 * -- For "Proof of precedent", with regards to available patterns for
+	 * transaction management in a database-connected log appender, refer to
+	 * org.apache.log4j.jdbc.JDBCAppender
+	 * 
+	 * -- Also note Appender#doAppend(...) as defined in AppenderSkeleton.
+	 * Ideally, here in HibernateAppender the method would be "Advised" such
+	 * that a transaction would be ensured as to be initialized before the call
+	 * to the superclass doAppend(...), and then flushed after the call returns.
+	 */
+
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * @return false, constantly
+	 */
 	@Override
 	public boolean requiresLayout() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	protected void append(LoggingEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
+	public synchronized void doAppend(LoggingEvent event) {
+		Session s = ensureTransactionStart();
+		super.doAppend(event);
+		s.flushIfWasNewInThisMethod();
+	}
+
+	
 }
--- module/brontes/module/brontes-hibernate-log4j/src/test/java/ws/gazebo/brontes/logging/hibernate/test/TestApp.java
+++ module/brontes/module/brontes-hibernate-log4j/src/test/java/ws/gazebo/brontes/logging/hibernate/test/TestApp.java
@@ -6,11 +6,14 @@ public class TestApp {
 	// commands in parallel.
 	//
 	// This TestApp class uses a separate thread for the HSQLDB database
-	
+
 	public static void main(String[] args) {
 		final String database = args[0];
 
 		Thread db = new Thread() {
+			// Note that the sever uses Log4J
+			// The file log4j.properties defines a console appender,
+			// for developer review of console output from the server
 			public void run() {
 				org.hsqldb.server.Server.main(new String[] { "--database.0",
 						database });
