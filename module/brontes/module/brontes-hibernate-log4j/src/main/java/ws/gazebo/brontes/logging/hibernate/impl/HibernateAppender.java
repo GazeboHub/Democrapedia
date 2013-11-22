04e92a3310b2bad515367a9282a350bec25f2eae
Sean Champ <spchamp+git-muletop@me.com>
2013-11-21 18:49:47 -0800

    add HibernateAppender stub (log4J)

---
+++ module/brontes/module/brontes-hibernate-log4j/src/main/java/ws/gazebo/brontes/logging/hibernate/impl/HibernateAppender.java
@@ -0,0 +1,26 @@
+package ws.gazebo.brontes.logging.hibernate.impl;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class HibernateAppender extends AppenderSkeleton {
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean requiresLayout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void append(LoggingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
